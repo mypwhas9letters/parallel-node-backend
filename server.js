1324f0bcbe1fdeda860265d184c6aa2ef556cb9f
Hui Wang <huiw3939@gmail.com>
2018-04-20 01:34:23 -0400

    creating controllers, models, and routes

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log('todo list RESTful API server started on: ' + port);
